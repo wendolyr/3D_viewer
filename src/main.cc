3d7563346758f9771121a550e912e10b89f12952
Chovie Tinkerer <chovieti@un-j1.kzn.21-school.ru>
2025-07-29 15:21:06 +0300

    Front без главного OpenGL виджета + парсер obj файла

---
+++ src/main.cc
@@ -0,0 +1,24 @@
+#include "mainwidget.h"
+
+#include <QApplication>
+
+int main(int argc, char* argv[]) {
+    QApplication app(argc, argv);
+
+    // Настройка OpenGL
+    // QSurfaceFormat format;
+    // format.setVersion(4, 1);
+    // format.setProfile(QSurfaceFormat::CoreProfile);
+    // format.setDepthBufferSize(24);
+    // format.setSamples(4);
+    // format.setSwapInterval(1);
+    // QSurfaceFormat::setDefaultFormat(format);
+
+    // Создание и отображение главного окна
+    MainWidget window;
+    window.setWindowTitle("3D_Viewer");
+    window.resize(1200, 800);
+    window.show();
+
+    return app.exec();
+}
