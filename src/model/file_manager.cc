e0608e6010c5f7e4d19e1b438bdf92461c85914d
Wendolyn Reyne <wendolyr@va-b5.kzn.21-school.ru>
2025-08-08 14:48:58 +0300

    modified parser; avg parse time for bugatti.obj(744213 vertices and 1501845 polygons) ~5 sec

--- src/model/file_manager.cc
+++ src/model/file_manager.cc
@@ -15,34 +15,66 @@ int FileManager::ParseFile(std::string &file_name, FigureModel &model) {
   std::set<std::pair<unsigned, unsigned>> polygons;
 
   while (std::getline(file, line)) {
-    std::istringstream iss(line);
-    std::string prefix;
-    iss >> prefix;
+    const char *ptr = &line[0];
 
-    if (prefix == "v") {
-      Vertex v;
-      if (!(iss >> v.x >> v.y >> v.z)) {
+    if (*ptr == 'v' && *(ptr + 1) == ' ') {
+      ++(++ptr);
+      std::vector<double> nums(3, 0.0);
+      char *end;
+      for (size_t i = 0; i < nums.size(); ++i) {
+        nums[i] = std::strtof(ptr, &end);
+        if (*end != ' ' && *end != '\0') {
+          return 2;
+        }
+
+        ptr = end;
+      }
+
+      if (*end != '\0') {
         return 2;
       }
-      vertices.push_back(v);
 
-    } else if (prefix == "f") {
+      vertices.emplace_back(nums[0], nums[1], nums[2]);
+
+    } else if (*ptr == 'f' && *(ptr + 1) == ' ') {
+      ++(++ptr);
       std::vector<unsigned> face;
-      std::string st;
-      while (iss >> st) {
-        int num;
-        auto [ptr, ec] = std::from_chars(st.data(), st.data() + st.size(), num);
 
-        if (ec != std::errc()) {
+      while (*ptr != '\0') {
+        if (*ptr == ' ' || *ptr == '\t') {
+          ++ptr;
+          continue;
+        }
+
+        unsigned num = 0;
+        bool negative = *ptr == '-';
+        if (negative) {
+          ++ptr;
+        }
+
+        if (*ptr >= '0' && *ptr <= '9') {
+          while (*ptr >= '0' && *ptr <= '9') {
+            num = num * 10 + (*ptr - '0');
+            ++ptr;
+          }
+        } else {
           return 2;
         }
 
-        bool negative = !st.empty() && st[0] == '-';
-        num = negative ? static_cast<int>(vertices.size()) - num : num - 1;
+        while (*ptr == '/' || (*ptr >= '0' && *ptr <= '9')) {
+          ++ptr;
+        }
 
-        if (num < 0 || num >= static_cast<int>(vertices.size())) {
+        if (*ptr != ' ' && *ptr != '\0') {
           return 2;
         }
+
+        num = negative ? static_cast<unsigned>(vertices.size()) - num : num - 1;
+
+        if (num >= static_cast<unsigned>(vertices.size())) {
+          return 2;
+        }
+
         face.push_back(num);
       }
 
@@ -64,140 +96,6 @@ int FileManager::ParseFile(std::string &file_name, FigureModel &model) {
 
   return 0;
 }
-
-// int MainWidget::LoadModelData(const QString &file_path) {
-//   while (ptr < end) {
-//     // Пропуск пробелов и управляющих символов
-//     while (ptr < end &&
-//            (*ptr == ' ' || *ptr == '\t' || *ptr == '\r' || *ptr == '\n')) {
-//       ++ptr;
-//     }
-//     if (ptr >= end) break;
-//     // Обработка вершин (строки начинающиеся с "v ")
-//     if (*ptr == 'v' && (ptr + 1 < end) &&
-//         (*(ptr + 1) == ' ' || *(ptr + 1) == '\t')) {
-//       if (ptr + 2 >= end) break;
-//       const char *line_start = ptr + 2;
-//       float x, y, z;
-//       char *next;  // Указатель на следующую позицию
-//       // Парсинг координаты X
-//       const char *before_x = line_start;
-//       x = std::strtof(line_start, &next);
-//       y = std::strtof(next, &next);
-//       z = std::strtof(next, &next);
-
-//       if (next == line_start) {
-//         QMessageBox::warning(this, "Ошибка", "Некорректный файл");
-//         return 1;
-//       }
-//       // Добавление вершины
-//       vertices.append(QVector3D(x, y, z));
-//       ptr = SkipToNextLine(ptr, end);
-//       continue;
-//     }
-//     // New Обработка граней (f)
-//     else if (*ptr == 'f' && (ptr + 1 < end) &&
-//              (*(ptr + 1) == ' ' || *(ptr + 1) == '\t')) {
-//       if (ptr + 2 >= end) break;
-
-//       const char *line_start = ptr + 2;
-//       face.clear();
-
-//       // Парсинг каждого значения в строке грани
-//       const char *token_start = line_start;
-//       // while (token_start < end && *token_start != '\n' && *token_start !=
-//       // '\r') {
-//       while (token_start < end) {
-//         // Пропуск пробелов в начале токена
-//         while (token_start < end &&
-//                (*token_start == ' ' || *token_start == '\t')) {
-//           ++token_start;
-//         }
-//         if (token_start >= end || *token_start == '\n' || *token_start ==
-//         '\r')
-//           break;
-//         // new variant
-//         int idx = 0;
-//         bool negative = false;
-//         const char *num_start = token_start;
-
-//         // Обработка знака
-//         if (*num_start == '-') {
-//           negative = true;
-//           ++num_start;
-//         } else if (*num_start == '+') {
-//           ++num_start;
-//         }
-
-//         // Парсинг числа
-//         if (num_start < end && *num_start >= '0' && *num_start <= '9') {
-//           while (num_start < end && *num_start >= '0' && *num_start <= '9') {
-//             idx = idx * 10 + (*num_start++ - '0');
-//           }
-
-//           // Преобразование индекса
-//           unsigned vertex_index;
-//           if (negative) {
-//             vertex_index = static_cast<unsigned>(vertices.size() - idx);
-//           } else {
-//             vertex_index = static_cast<unsigned>(idx - 1);
-//           }
-
-//           // Проверка и добавление индекса
-//           if (vertex_index < static_cast<unsigned>(vertices.size())) {
-//             face.push_back(vertex_index);
-//           }
-//         }
-
-//         while (token_start < end && *token_start != ' ' &&
-//                *token_start != '\t' && *token_start != '\n' &&
-//                *token_start != '\r') {
-//           ++token_start;
-//         }
-//       }
-
-//       // Сохраняем грань если есть хотя бы 3 вершины
-//       const size_t face_size = face.size();
-//       // if (face.size() >= 2) {
-//       if (face_size >= 2) {
-//         // Для каждой вершины в грани
-//         // new variant
-//         const unsigned int last_index = face[face_size - 1];
-//         unsigned int prev_index = last_index;
-
-//         for (size_t i = 0; i < face_size; ++i) {
-//           const unsigned int current_index = face[i];
-
-//           // Создание упорядоченной пары
-//           const auto edge = (prev_index < current_index)
-//                                 ? qMakePair(prev_index, current_index)
-//                                 : qMakePair(current_index, prev_index);
-
-//           edges.insert(edge);
-//           prev_index = current_index;
-//         }
-//       }
-
-//       ptr = SkipToNextLine(ptr, end);
-//       continue;
-//     }
-//     // Пропуск остальных строк
-//     else {
-//       ptr = SkipToNextLine(ptr, end);
-//     }
-//     // Переход к следующему символу
-//     if (ptr < end) ++ptr;
-//   }
-
-//   QVector<QPair<unsigned, unsigned>> edgeVec;
-//   edgeVec.reserve(edges.size());
-//   for (const auto &edge : edges) {
-//     edgeVec.append(edge);
-//   }
-
-//   return 0;
-// }
-// //============
 
 void FileManager::LoadLastState(FigureModel &model) {
   std::string last_object_file = ".last_object.obj";
@@ -235,8 +133,8 @@ void FileManager::SaveModel(std::string &file_name, FigureModel &model) {
 }
 
 /**
- * настройки также должны будут сохранять и загружать толщину, цвет и тд линий и
- * вершин! позже доделать этот момент
+ * настройки также должны будут сохранять и загружать толщину, цвет и тд линий
+ * и вершин! позже доделать этот момент
  */
 void FileManager::SaveSettings(std::string &file_name, FigureModel &model) {
   std::ofstream file(file_name);
--- src/test.cc
+++ src/test.cc
@@ -8,37 +8,30 @@ int main() {
   std::string name = "bugatti.obj";
   // std::string name = "cube.obj";
 
-  facade.ParseFile(name);
-
-  // for (int i = 2; i < 120; ++i) {
-  // auto s = facade.GetVertices();
-  // for (auto i : s) {
-  //   std::cout << i.x << ' ' << i.y << ' ' << i.z << std::endl;
-  // }
-
-  s21::Vertex a = {1, 1, 1};
   auto start = std::chrono::steady_clock::now();
-  for (int i = 0; i < 120; ++i) {
-    // facade.MoveFigure(a);
-    // facade.RotateFigure(a);
-    facade.ScaleFigure(2);
-  }
-
+  facade.ParseFile(name);
   auto end = std::chrono::steady_clock::now();
-
   auto duration =
       std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
 
   std::cout << "Time " << duration.count() << " ms" << std::endl;
 
-  // s = facade.GetVertices();
-
-  // for (auto i : s) {
-  //   std::cout << i.x << ' ' << i.y << ' ' << i.z << std::endl;
+  // s21::Vertex a = {1, 1, 1};
+  // for (int i = 0; i < 120; ++i) {
+  //   // facade.MoveFigure(a);
+  //   // facade.RotateFigure(a);
+  //   facade.ScaleFigure(2);
   // }
 
   // std::cout << facade.GetVertices().size() << std::endl;
   // std::cout << facade.GetPolygons().size() << std::endl;
+
+  // auto v = facade.GetVertices();
+  // auto p = facade.GetPolygons();
+
+  // for (auto i : p) {
+  //   std::cout << i.first + 1 << ' ' << i.second + 1 << '\n';
+  // }
 
   return 0;
 }
